c3f0d247de2303f0b16876bfe61a703a61e54b6c
nighil manuel <nighilmanuel2307@gmail.com>
2022-09-12 17:41:36 +0530

    refresh token inserted to database

--- authServer.js
+++ authServer.js
@@ -3,37 +3,94 @@ require('dotenv').config();
 const express = require('express');
 const jwt = require('jsonwebtoken');
 const bodyParser = require('body-parser');
+const port = process.env.PORT || 4000 ;
+const conn = require('./conn');
 
 const app = express();
 
 //app.use(bodyParser.)
 app.use(bodyParser.json());
 
+let refreshTokens = [];
 
-app.post('/login', (req ,res) => {
+app.post('/token' , (req , res) => {
+    const refreshToken = req.body.token; 
+    if (refreshToken  == null ){
+        return res.sendStatus(401);
+    }
+    if (!refreshTokens.includes(refreshToken)) {
+        return res.sendStatus(403);
+    }
+    jwt.verify(refreshToken , process.env.REFRESH_TOKEN_SECRET , (err, user) => {
+        console.log(user);
+        const name = user.name;
+        if(err){
+            res.sendStatus(403);
+        }
+        const accessToken = generateAccessToken({ name: name });
+        console.log(user);
+        /*
+        const sql = 'UPDATE login SET access_token = ? WHERE username = hi ;'
+        conn.query(sql, [accessToken] , (err,result) => {
+            if (err) {
+                res.status(400).json({
+                    errorMessage: 'Error registering, try agin...',
+                    error: err
+                });
+            } else {
+                res.status(201).json({
+                    message: 'User registered successfully!'
+                })
+            }
+        })
+        */
+        return res.json({
+            accessToken: accessToken
+        })
+        
+    })
+})
+
+app.post('/register', (req ,res) => {
     // Autheticate user here
     
+    
+
+    //
     const username = req.body.username;
     const user = { name: username };
-    const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET);
-    res.json({ accessToken: accessToken });
+    const accessToken = generateAccessToken(user);
+    const refreshToken = jwt.sign(user, process.env.REFRESH_TOKEN_SECRET);
+    const sql = 'UPDATE login SET refresh_token = ?  WHERE username = "hi" ;' ;
+    conn.query(sql , [refreshToken] , (err, result) => {
+        console.log(refreshToken);
+        
+        if (err) {
+           return res.json({
+                errorMessage: 'Error registering, try agin...',
+                error: err
+            });
+        } else {
+            return res.json({
+                message: 'User registered successfully!'
+            })
+        }
+        
+       
+    });
+    //res.json({ 
+    //    accessToken: accessToken ,
+    //    refreshToken: refreshToken
+   // });
+    console.log('ede');
+    refreshTokens.push(refreshToken);
+   
+   
     console.log(accessToken);
 });
 
-function authenticateToken(req , res ,next ) {
-    const authHeader =  req.headers['authorization'];
-    const token = authHeader && authHeader.split(' ')[1];
-    if(token == null) {
-        return res.sendStatus(401)
-    }
-
-    jwt.verify( token , process.env.ACCESS_TOKEN_SECRET , (err, user) => {
-        if (err) {
-            return res.sendStatus(403);
-        }
-        req.user = user;
-        next();
-    })
+function generateAccessToken (user) {
+    return jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '20s'});
 }
 
-app.listen(4000);
+app.listen(port,console.log(`Authentication Server running on port : ` + port));
--- server.js
+++ server.js
@@ -3,6 +3,9 @@ require('dotenv').config();
 const express = require('express');
 const jwt = require('jsonwebtoken');
 const bodyParser = require('body-parser');
+const conn = require('./conn');
+
+const port = process.env.PORT || 3000 ;
 
 const app = express();
 
@@ -21,24 +24,26 @@ const post = [
 ]
 
 app.get('/posts', authenticateToken, (req ,res) => {
+    console.log(req.user)
     res.json(post.filter( post => post.name === req.user.name));
 })
 
 
-function authenticateToken(req , res ,next ) {
+function authenticateToken(req , res , next ) {
     const authHeader =  req.headers['authorization'];
     const token = authHeader && authHeader.split(' ')[1];
     if(token == null) {
         return res.sendStatus(401)
     }
-
+    //console.log(token);
     jwt.verify( token , process.env.ACCESS_TOKEN_SECRET , (err, user) => {
         if (err) {
             return res.sendStatus(403);
         }
         req.user = user;
+        //console.log(user);
         next();
     })
 }
 
-app.listen(3000);
+app.listen(port,console.log(`Server running on port : ` + port));
